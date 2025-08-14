50978ade306d5d1fe80aa682480e2ad09ec6f5d8
SanseL <sanselguh@gmail.com>
2025-08-14 15:05:45 +0700

    Add files via upload

---
+++ croissant_demo/src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, App, HttpServer, HttpResponse};
+
+#[get("/croissant_demo")]
+async fn croissant_demo() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html") // Set the content type to HTML
+        .body(include_str!("../static/index.html"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(croissant_demo)
+    })
+    .bind(("127.0.0.1", 4462))?
+    .run()
+    .await
+}
